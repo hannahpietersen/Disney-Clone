12eebd61d038cdc0529c2ddd63953116efbc42cb
someid <hannahpietersen@gmail.com>
2023-03-19 01:03:18 +0530

    react router updated for login.js

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,12 +11,15 @@ import {
   Link
   } from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">
      <Router>
       <Header />
       <Routes>
+        <Route path="/login" element={<Login />} />
+
         <Route path="/detail" element={<Detail />} />
          
         <Route path="/" element={<Home />} />
